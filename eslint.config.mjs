b42ae79f17ee594c6bb9c4bd1c2c115f8b93b3f3
Tun Cham Roeun <chamroeuntun25@outlook.com>
2026-04-24 10:37:08 +0700

    eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import antfu from '@antfu/eslint-config';
+
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(antfu(
+  {
+    type: 'app',
+    vue: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: 'single',
+    },
+  },
+  {
+    rules: {
+      'ts/no-redeclare': 'off',
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'antfu/no-top-level-await': ['off'],
+      'node/prefer-global/process': ['off'],
+      'node/no-process-env': ['off'],
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          tsconfig: {
+            rootDir: '.',
+          },
+        },
+      ],
+      'unicorn/filename-case': ['error', {
+        case: 'kebabCase',
+        ignore: ['README.md'],
+      }],
+    },
+  },
+  // ADD THIS BLOCK TO FIX THE CRASH
+  {
+    files: ['**/*.md/**', '**/*.md', '**/*.json'],
+    rules: {
+      'perfectionist/sort-imports': 'off',
+    },
+  },
+));
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
