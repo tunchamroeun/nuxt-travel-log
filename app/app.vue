f982881d9e3217eb2f6a100ce7fe6fc41cda460e
Tun Cham Roeun <chamroeuntun25@outlook.com>
2026-04-25 21:29:51 +0700

    tailwind / daisy ui / icons setup

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,94 @@
 <template>
   <div>
+    <div>
+      <button class="btn btn-active">
+        Default
+      </button>
+      <button class="btn btn-active btn-primary">
+        Primary
+      </button>
+      <button class="btn btn-active btn-secondary">
+        Secondary
+      </button>
+      <button class="btn btn-active btn-accent">
+        Accent
+      </button>
+      <button class="btn btn-active btn-info">
+        Info
+      </button>
+      <button class="btn btn-active btn-success">
+        Success
+      </button>
+      <button class="btn btn-active btn-warning">
+        Warning
+      </button>
+      <button class="btn btn-active btn-error">
+        Error
+      </button>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-sm">
+      <div class="card-body">
+        <span class="badge badge-xs badge-warning">Most Popular</span>
+        <div class="flex justify-between">
+          <h2 class="text-3xl font-bold">
+            Premium
+          </h2>
+          <span class="text-xl">$29/mo</span>
+        </div>
+        <ul class="mt-6 flex flex-col gap-2 text-xs">
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+            <span>High-resolution image generation</span>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+            <span>Customizable style templates</span>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+            <span>Batch processing capabilities</span>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+            <span>AI-driven image enhancements</span>
+          </li>
+          <li class="opacity-50">
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+            <span class="line-through">Seamless cloud integration</span>
+          </li>
+          <li class="opacity-50">
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+            <span class="line-through">Real-time collaboration tools</span>
+          </li>
+        </ul>
+        <div class="mt-6">
+          <button class="btn btn-primary btn-block">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        >
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          Card Title
+        </h2>
+        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">
+            View Map
+            <Icon name="tabler:map" />
+          </button>
+        </div>
+      </div>
+    </div>
+
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
     <button>Hello</button>
